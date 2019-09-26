e4e441a632a24e4f2089af597c05f53fc3123320
DeltaSlim <geb.ermias@gmail.com>
2019-09-26 11:24:05 -0400

    1 file modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LiveComponent } from './live/live.component';
+import { FooterComponent } from './footer/footer.component';
+import { HttpService } from './http.service';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule, ],
-  declarations: [ AppComponent, HelloComponent, NavbarComponent],
+  declarations: [ AppComponent, HelloComponent, NavbarComponent, LiveComponent, FooterComponent],
   bootstrap:    [ AppComponent ],
-  providers: []
+  providers: [HttpService]
 })
 export class AppModule { }
