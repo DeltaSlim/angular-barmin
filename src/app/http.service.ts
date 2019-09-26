acfa0d199ee79f1f5ea61ce2fa2bdcb3ca74ac00
DeltaSlim <geb.ermias@gmail.com>
2019-09-26 12:50:00 -0400

    1 file modified

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable()
 export class HttpService {
@@ -7,7 +7,17 @@ export class HttpService {
   constructor(private http: HttpClient) { }
 
 
-  getLiveGames() {
-    return this.http.get('https://barmin.cfapps.io/matches');
+  getLiveGames(params?:HttpParams) {
+    params = params ? params : new HttpParams();
+    if (!params.has('projection')) {
+        params.append('projection', 'results');
+
+    }
+    params.append('isFinished', 'false');
+    const date = new Date();
+    params = params.append('matchTime','1970-01-01T00:00:00Z');
+    params = params.append('matchTime',date.toISOString());
+    params = params.append('sort','matchTime');
+    return this.http.get('https://barmin.cfapps.io/matches', {params: params});
   }
 }
