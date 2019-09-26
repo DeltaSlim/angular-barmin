7b3047350e7502c23a499d3de3c74aed8197bc12
DeltaSlim <geb.ermias@gmail.com>
2019-09-26 09:46:41 -0400

    3 files modified, 26 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { MatchesComponent } from './components/matches/matches.component';
-import { MatchService } from './services/match.service';
-import { FooterComponent } from './components/footer/footer.component';
-import { FeaturedComponent } from './components/featured/featured.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, NavbarComponent, MatchesComponent, FooterComponent, FeaturedComponent, HomeComponent, LoginComponent, RegisterComponent],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, ],
+  declarations: [ AppComponent, HelloComponent],
   bootstrap:    [ AppComponent ],
-  providers: [MatchService]
+  providers: []
 })
 export class AppModule { }
